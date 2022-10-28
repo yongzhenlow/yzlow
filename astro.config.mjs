328649664d4a4ba0c445d05680caaf55b9aeb628
YongZhen Low <cephyz@gmail.com>
2022-10-28 17:43:17 +0800

    chore: update astro image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()]
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
