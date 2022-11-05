f32e45cf516c4121efabc8e01de9bfa20f81ef44
YongZhen Low <cephyz@gmail.com>
2022-11-06 02:00:00 +0800

    feat: add default favicon.ico
    This fixes issues where browser tries to fetch favicon in xml files

---
+++ src/pages/favicon.ico.ts
@@ -0,0 +1,18 @@
+import { settings } from '@store/settings';
+import type { APIRoute } from 'astro';
+
+// @ts-ignore might be a typing issue, since EndpointOutput requires body to be a string
+// but docs show otherwise and this works.
+export const get: APIRoute = async () => {
+  if (settings.icon) {
+    const response = await fetch(settings.icon);
+    const buffer = Buffer.from(await response.arrayBuffer());
+
+    return {
+      body: buffer,
+      encoding: 'binary',
+    };
+  }
+
+  return { body: '' };
+};
