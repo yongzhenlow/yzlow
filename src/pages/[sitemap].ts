bf269795a529ecb4e988925e04974a03a04a4703
YongZhen Low <cephyz@gmail.com>
2022-11-06 01:28:08 +0800

    feat: combine sitemaps xml and xsl with the fix in https://github.com/withastro/astro/pull/5258

--- src/pages/[sitemap].ts
+++ src/pages/[sitemap].ts
@@ -3,23 +3,25 @@ import type { APIRoute } from 'astro';
 export const get: APIRoute = async ({ params }) => {
   const apiURL = new URL(import.meta.env.GHOST_CONTENT_API_URL);
   const siteURL = new URL(import.meta.env.SITE_URL ?? import.meta.env.SITE);
-  const res = await fetch(new URL(`${params.sitemap}.xml`, apiURL));
+  const res = await fetch(new URL(`${params.sitemap}`, apiURL));
   const raw = await res.text();
   const body = raw
     .replaceAll(apiURL.href, siteURL.href)
     .replaceAll(apiURL.host, siteURL.host);
 
-  return {
-    body,
-  };
+  return new Response(body, {
+    status: 200,
+    headers: { 'content-type': `application/xml` },
+  });
 };
 
 export function getStaticPaths() {
   return [
-    'sitemap',
-    'sitemap-pages',
-    'sitemap-posts',
-    'sitemap-authors',
-    'sitemap-tags',
+    'sitemap.xml',
+    'sitemap.xsl',
+    'sitemap-pages.xml',
+    'sitemap-posts.xml',
+    'sitemap-authors.xml',
+    'sitemap-tags.xml',
   ].map((sitemap) => ({ params: { sitemap } }));
 }
